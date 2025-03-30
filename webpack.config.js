fd9a4b28a0f11c853344331a41e6fc7082cc5b68
Winston Hanun Júnior <devjuniorhanun@gmail.com>
2025-03-30 14:31:36 -0300

    Inciando o Html no WebPack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 // Pacote para ajustar o caminho do arquivo
 const path = require("path");
 
+//Importação do plugin de importação do HTML
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  target:"web",
-  mode:"development",
-  entry:path.resolve(__dirname, "src","js","main.js"), 
-  output:{
-    filename:"main.js",
-    path:path.resolve(__dirname, "dist") 
-  }, 
-}
+  target: "web",
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "js", "main.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+  plugins:[
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname,"index.html"),
+    }),
+  ]
+};
