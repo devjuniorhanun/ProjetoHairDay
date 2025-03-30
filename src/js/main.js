174456d5dda324f09fbf613cc058472f1ff691f2
Winston Hanun Júnior <devjuniorhanun@gmail.com>
2025-03-30 14:41:45 -0300

    Inciando os Css no WebPack

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+"use strict"
+
+//css
+import "../css/global.css"
+import "../css/form.css"
+import "../css/schedule.css"
--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,32 @@ module.exports = {
     open: true,
     liveReload: true,
   },
-  plugins:[
+  plugins: [
     new HTMLWebpackPlugin({
-      template: path.resolve(__dirname,"index.html"),
+      template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve("src", "assets", "scissors.svg"),
     }),
-  ]
+  ],
+  module: {
+    rules: [
+      //Para o bloco abaixo instalei
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        exclude: "/node_modules",
+      },
+      //Para o bloco abaixo instalei
+      //npm i babel-loader@9.1.3 @babel/core@7.23.7 @babel/preset-env@7.23.7 --save-dev
+      {
+        test: /\.js$/i,
+        exclude: "/node_modules",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
 };
